79e74179c92c1d8ad774128dac6df17d0f1ab4d7
eprei <93380002+eprei@users.noreply.github.com>
2022-04-08 17:15:19 +0200

    its working

--- Makefile
+++ Makefile
@@ -1,2 +1,36 @@
-all:
-	cc -I ./minilibx_opengl_20191021  so_long.c -L ./minilibx_opengl_20191021/ -lmlx -framework OpenGL -framework AppKit -o so_long
+
+NAME = so_long
+
+NAMELIBFT = libft.a
+
+SRCS = so_long.c
+
+OBJ = $(SRCS:.c=.o)
+
+CFLAGS = -Wall -Wextra -Werror
+
+CC = gcc
+
+LIBFTPATH = ./libft/
+
+MLXPATH = ./minilibx_opengl_20191021
+
+all: $(NAME)
+
+$(NAME):
+	make -C $(LIBFTPATH)
+	make -C $(MLXPATH)
+	CC -I .$(MLXPATH) so_long.c -L $(MLXPATH) $(LIBFTPATH)$(NAMELIBFT) -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+
+clean:
+	rm -rf $(OBJ)
+	make clean -C $(LIBFTPATH)
+	make clean -C $(MLXPATH)
+
+fclean: clean
+	rm -rf $(NAME)
+	make fclean -C $(LIBFTPATH)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- so_long.h
+++ so_long.h
@@ -1,9 +1,14 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "mlx.h"
+# include "minilibx_opengl_20191021/mlx.h"
 # include <unistd.h>
 # include <stdlib.h>
+# include "libft/libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include <math.h>
 
 # define LEFT 0
 # define RIGHT 2
@@ -15,15 +20,46 @@ typedef struct s_game
 {
     void *mlx_ptr;
     void *win_ptr;
-	void *img_char_ptr;
-	void *img_wall_ptr;
-	void *img_items_ptr;
-	void *img_exit_ptr;
+	int win_size_x;
+	int win_size_y;
+	char **tab;
+	void *img_dog;
+	void *img_empty;
+	void *img_wall;
+	void *img_item;
+	void *img_exit;
+	char *dog_texture;
+	char *empty_texture;
+	char *wall_texture;
+	char *item_texture;
+	char *exit_texture;
     int img_res;
 	unsigned int count_movs;
-	unsigned int pos_char_x;
-	unsigned int pos_char_y;
-	char *dog_texture;
+	char *count_str;
+	int pos_dog_x;
+	int pos_dog_y;
+	int map_length;
+	int map_width;
+	unsigned int count_c;
+	int count_e;
+	int count_p;
 } t_game;
+
+int	key_ctrl(int key, t_game *game_ptr);
+void	ft_check_args(int argc, char **argv);
+void	ft_game_init(t_game *game_ptr);
+void	ft_exit(t_game *game_ptr, char *error_message, int exit_status);
+int	main(int argc, char **argv);
+void	ft_count_lines(t_game *game_ptr, char *argv1);
+size_t	ft_strlen_without_new_line(const char *s);
+int	ft_check_rectangle(t_game *game_ptr);
+void	ft_get_map(t_game *game_ptr, char *argv1);
+int	ft_check_walls(t_game *game_ptr);
+void	ft_check_characters(t_game *game_ptr);
+void	ft_process_map(t_game *game_ptr, char *argv1);
+void	ft_update_win_size(t_game *game_ptr);
+void ft_init_images(t_game *game_ptr);
+void	fn_put_tab_to_window(t_game *game_ptr);
+void ft_process_mov(t_game *game_ptr, int x, int y);
 
 #endif
